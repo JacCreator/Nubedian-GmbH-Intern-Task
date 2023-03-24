e5c1b9dbd38086a531130c60f2ca82cb3f413d1a
Kopyto <94701397+JacCreator@users.noreply.github.com>
2023-03-24 21:07:46 +0100

    First commit

---
+++ src/main/java/com/webapp/cpudemo/CpudemoApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.cpudemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpudemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpudemoApplication.class, args);
+	}
+
+}
