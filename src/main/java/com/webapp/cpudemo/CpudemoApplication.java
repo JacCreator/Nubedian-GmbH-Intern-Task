1eccfd20baf8910253f5cd2ac8f8584405afcf1f
Kopyto <JacCreator@users.github.com>
2023-03-30 15:13:31 +0200

    Change datasource env to default values

--- src/main/java/com/webapp/cpudemo/CpudemoApplication.java
+++ src/main/java/com/webapp/cpudemo/CpudemoApplication.java
@@ -20,23 +20,5 @@ public class CpudemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CpudemoApplication.class, args);
-
-
-		// add one-to-many relationship test
-		/* ConfigurableApplicationContext configurableApplicationContext =
-				SpringApplication.run(CpudemoApplication.class, args);
-		SocketRepository socketRepository = configurableApplicationContext.getBean(SocketRepository.class);
-
-		Socket socket = new Socket("Socket 1700");
-		Cpu cpu1 = new Cpu("Intel", "Core i5-13500", new BigDecimal("2.5"), new BigDecimal("4.8"),
-				14, 20, new BigDecimal("75"), new BigDecimal("1319.00"), socket);
-		Cpu cpu2 = new Cpu("Intel", "Core i5-13500", new BigDecimal("2.5"), new BigDecimal("4.8"),
-				14, 20, new BigDecimal("75"), new BigDecimal("1319.00"), socket);
-		Set<Cpu> cpus = new HashSet<>();
-		cpus.add(cpu1);
-		cpus.add(cpu2);
-		socket.setCpus(cpus);
-		socketRepository.save(socket);*/
 	}
-
 }
